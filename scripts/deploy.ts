e8908b04a1e21c3dae27f3d88f8200f799cdfcfe
nathan gd <nathan.gardet-derc@pm.me>
2022-05-31 11:03:28 +0200

    better

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,17 +1,13 @@
 import { ethers } from "hardhat";
 // scripts/deploy.js
 async function deploy() {
-   // 1. Get the contract to deploy
    const MyNFT = await ethers.getContractFactory('NFTCollectible');
    console.log('Deploying MyNFT...');
 
-   // 2. Instantiating a new Box smart contract
    const mynft = await MyNFT.deploy();
 
-   // 3. Waiting for the deployment to resolve
    await mynft.deployed();
 
-   // 4. Use the contract instance to get the contract address
    console.log('MyNFT deployed to:', mynft.address);
 }
 
